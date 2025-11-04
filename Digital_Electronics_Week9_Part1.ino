7197d70afe2b24482e7c4d85b1f06de558a1a798
Leeben315 <leebeniam@gmail.com>
2025-11-04 13:15:36 -0500

    Back to 4 buttons

--- Digital_Electronics_Week9_Part1.ino
+++ Digital_Electronics_Week9_Part1.ino
@@ -2,11 +2,13 @@
 
 int button1Pin = 32;
 int button2Pin = 30;
-
+int button3Pin = 28;
+int button4Pin = 26;
 
 BetterButton button1(button1Pin, 0);
 BetterButton button2(button2Pin, 1);
-
+BetterButton button3(button3Pin, 2);
+BetterButton button4(button4Pin, 3);
 
 int midiNotes[4] = { 60, 64, 67, 71};
 
@@ -17,13 +19,17 @@ void setup() {
   button1.releaseHandler(onRelease);
   button2.pressHandler(onPress);
   button2.releaseHandler(onRelease);
-
+  button3.pressHandler(onPress);
+  button3.releaseHandler(onRelease);
+  button4.pressHandler(onPress);
+  button4.releaseHandler(onRelease);
 }
 
 void loop() {
   button1.process();
   button2.process();
-
+  button3.process();
+  button4.process();
 }
 
 void onPress(int val) {
